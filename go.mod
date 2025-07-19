1d580e3d5dc099023d516f98f45b5b101ec73528
psxzz <m.martusevich@yandex.ru>
2025-07-19 21:25:50 +0300

    fix: optimistic locking for transaction

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/psxzz/dmsecret-backend
 go 1.24.2
 
 require (
+	github.com/avast/retry-go/v4 v4.6.1
 	github.com/gin-gonic/gin v1.10.1
 	github.com/google/uuid v1.6.0
 	github.com/oapi-codegen/gin-middleware v1.0.2
--- internal/repository/secrets/secrets.go
+++ internal/repository/secrets/secrets.go
@@ -2,9 +2,12 @@ package secrets
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"strconv"
+	"time"
 
+	"github.com/avast/retry-go/v4"
 	"github.com/google/uuid"
 	"github.com/valkey-io/valkey-go"
 )
@@ -52,57 +55,75 @@ func (r *secretsRepository) GetByID(ctx context.Context, secretID uuid.UUID) (st
 
 	key := getSecretKey(secretID)
 
-	resp := client.Do(ctx, client.B().Watch().Key(key).Build())
-	if err := resp.Error(); err != nil {
-		return "", fmt.Errorf("couldn't watch key: %w", err)
-	}
-
-	resp = client.Do(ctx, client.B().Exists().Key(key).Build())
-	if err := resp.Error(); err != nil {
-		return "", fmt.Errorf("couldn't check key existance: %w", err)
-	}
-
-	exists, err := resp.AsBool()
-	if err != nil {
-		return "", fmt.Errorf("couldn't get bool from response: %w", err)
-	}
-
-	if !exists {
-		return "", ErrNotFound
-	}
-
-	resps := client.DoMulti(
-		ctx,
-		client.B().Multi().Build(),
-		client.B().Hincrby().Key(key).Field(hashFieldSeenCount).Increment(-1).Build(),
-		client.B().Exec().Build(),
-	)
-	for _, r := range resps {
-		if err := r.Error(); err != nil {
-			return "", fmt.Errorf("couldn't do pipelined: %w", err)
+	getter := func() (string, error) {
+		resp := client.Do(ctx, client.B().Watch().Key(key).Build())
+		if err := resp.Error(); err != nil {
+			return "", fmt.Errorf("couldn't watch key: %w", err)
 		}
+
+		resp = client.Do(ctx, client.B().Exists().Key(key).Build())
+		if err := resp.Error(); err != nil {
+			return "", fmt.Errorf("couldn't check key existence: %w", err)
+		}
+
+		exists, err := resp.AsBool()
+		if err != nil {
+			return "", fmt.Errorf("couldn't get bool from response: %w", err)
+		}
+
+		if !exists {
+			return "", ErrNotFound
+		}
+
+		resps := client.DoMulti(
+			ctx,
+			client.B().Multi().Build(),
+			client.B().Hincrby().Key(key).Field(hashFieldSeenCount).Increment(-1).Build(),
+			client.B().Exec().Build(),
+		)
+		for _, r := range resps {
+			if err := r.Error(); err != nil {
+				return "", fmt.Errorf("couldn't do multi pipeline: %w", err)
+			}
+		}
+
+		resp = client.Do(ctx, client.B().Hgetall().Key(key).Build())
+		if err := resp.Error(); err != nil {
+			return "", fmt.Errorf("couldn't get key: %w", err)
+		}
+
+		hash, err := resp.AsStrMap()
+		if err != nil {
+			return "", fmt.Errorf("couldn't get hash from response: %w", err)
+		}
+
+		seenCount, err := strconv.Atoi(hash[hashFieldSeenCount])
+		if err != nil {
+			return "", fmt.Errorf("couldn't convert seen counter: %w", err)
+		}
+
+		if seenCount == 0 {
+			resp := client.Do(ctx, client.B().Unlink().Key(key).Build())
+			if err := resp.Error(); err != nil {
+				return "", fmt.Errorf("couldn't unlink: %w", err)
+			}
+		}
+
+		return hash[hashFieldPayload], nil
 	}
 
-	resp = client.Do(ctx, client.B().Hgetall().Key(key).Build())
-	if err := resp.Error(); err != nil {
-		return "", fmt.Errorf("couldn't get key: %w", err)
-	}
-
-	hash, err := resp.AsStrMap()
+	payload, err := retry.DoWithData(
+		getter,
+		retry.Context(ctx),
+		retry.Attempts(5),
+		retry.Delay(10*time.Millisecond),
+		retry.RetryIf(func(err error) bool {
+			return !errors.Is(err, ErrNotFound)
+		}),
+	)
 	if err != nil {
-		return "", fmt.Errorf("couldn't get hash from response: %w", err)
+		return "", fmt.Errorf("couldn't get by id: %w", err)
 	}
 
-	seenCount, err := strconv.Atoi(hash[hashFieldSeenCount])
-	if err != nil {
-		return "", fmt.Errorf("couldn't convert seen counter: %w", err)
-	}
-
-	if seenCount < 0 {
-		client.Do(ctx, client.B().Unlink().Key(key).Build())
-
-		return "", ErrNotFound
-	}
-
-	return hash[hashFieldPayload], nil
+	return payload, nil
 }
