9a2e767210022a81a6c5b96e938642481f2b7ee5
Daniil Avtushko <adanilcoder@gmail.com>
2025-10-03 18:51:17 +0300

    fix cors

--- internal/server/middlewares/cors.go
+++ internal/server/middlewares/cors.go
@@ -9,7 +9,7 @@ import (
 //nolint:lll
 func WithCORSCheck() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*") // "https://ghostylink.algostack.ru" "http://localhost:5173"
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://ghostylink.algostack.ru") // "https://ghostylink.algostack.ru" "http://localhost:5173"
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
